e6db3079d1cf9170bb34109caa9fa41f88a377e9
Chad Nale <chad.nale@gmail.com>
2014-10-28 19:57:57 -0400

    added banner decoding / testing page

---
+++ testdb.php
@@ -0,0 +1,23 @@
+<?php
+require "site.inc";
+?>
+<html>
+<head>
+<title>Test Page</title>    
+    
+</head>
+<body>
+Here is the data : <br>
+<div>
+<?php 
+
+// lets get geocode info
+calculateCorrectNFLTeamBanner();
+
+?>    
+    
+</div>
+    
+</body>
+    
+</html>
