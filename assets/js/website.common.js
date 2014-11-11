3f279a5880252f4a86dd5a61f8ede53d2db7a982
Brian <Brian@bmcwebdev.com>
2014-11-10 20:24:29 -0500

    alert message style

--- assets/js/website.common.js
+++ assets/js/website.common.js
@@ -131,6 +131,8 @@ function validateForm()
                     }
                     else if (0 < data.indexOf("Sweepstakes Closed")) {
                         $("#processingresult").html("Please come back when the sweepstakes is available -- good luck!");
+						$("#processingresult").css({"color":"#e1e000", "font-family":"bose_gothic_screenMdOb", "font-size":"24px", "display":"inline-block"})
+
                     }
                 }
             });
